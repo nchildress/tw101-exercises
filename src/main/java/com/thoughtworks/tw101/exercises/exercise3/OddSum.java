ec1d7d3b6645d3520c3b1c47a1042327fe66535d
nchildress <nchildress@smu.edu>
2017-03-13 17:41:33 -0500

    of function sums low to high

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -3,6 +3,10 @@ package com.thoughtworks.tw101.exercises.exercise3;
 public class OddSum {
 
     public int of(int start, int end) {
-        return -1;
+        int sum = 0;
+        for(int low = start; low < end; low += 2){
+            sum += low;
+        }
+        return sum;
     }
 }

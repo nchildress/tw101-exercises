2df8c6176629728610824f468bdefccb63abce41
nchildress <nchildress@smu.edu>
2017-03-13 14:41:04 -0500

    accumulator class

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,14 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+
+    private int total = 0;
+
+    public void increment(){
+        total++;
+    }
+
+    public void total(){
+        System.out.println(total);
+    }
 }

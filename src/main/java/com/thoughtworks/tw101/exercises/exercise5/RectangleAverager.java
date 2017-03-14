38bf4582645f363711577a160e94d7bea43e5586
nchildress <nchildress@smu.edu>
2017-03-13 19:46:02 -0500

    average area of rectangles

--- src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
@@ -3,6 +3,11 @@ package com.thoughtworks.tw101.exercises.exercise5;
 public class RectangleAverager {
 
     public float averageArea(Rectangle[] rectangles) {
-        return 0;
+        int sum = 0;
+        for (Rectangle rectangle : rectangles){
+            sum += rectangle.area();
+        }
+        float average = sum/rectangles.length;
+        return average;
     }
 }
